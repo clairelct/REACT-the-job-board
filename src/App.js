417f52f1ad6825c78d32359a1c384e5ca2e0c4da
Claire Leconte <claire.lcnt@gmail.com>
2020-11-02 15:24:02 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,87 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Jobs from "./components/Jobs";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <div className="container">
+        <Header className="bloc-titre" title="The Job Board" />
+
+        <div class="container-box">
+          <Jobs
+            className="red"
+            title="Full Time Sales Associate - Sydney Boutique"
+            contractType="CDI"
+            country="Australie"
+            city="Sydney"
+          />
+          <Jobs
+            className="green"
+            title="Agent de Sécurité - Pantin"
+            contractType="CDI"
+            country="France"
+            city="Pantin"
+          />
+          <Jobs
+            className="yellow"
+            title="Responsable d'Atelier (H/F)"
+            contractType="CDD"
+            country="France"
+            city="Paris"
+          />
+          <Jobs
+            className="blue"
+            title="Chef de Projets"
+            contractType="CDD"
+            country="France"
+            city="Paris"
+          />
+          <Jobs
+            className="pink"
+            title="Développeur React.js"
+            contractType="CDI"
+            country="France"
+            city="Paris"
+          />
+          <Jobs
+            className="red"
+            title="Sales Associate Stockholm"
+            contractType="CDI"
+            country="Suède"
+            city="Stockholm"
+          />
+          <Jobs
+            className="green"
+            title="Vendeur/se - Crans Montana"
+            contractType="CDI"
+            country="Suisse"
+            city="Crans-Montana"
+          />
+          <Jobs
+            className="yellow"
+            title="CRM &amp; Data Quality Analyst"
+            contractType="CDI"
+            country="USA"
+            city="New York"
+          />
+          <Jobs
+            className="blue"
+            title="Infirmier (H/F)"
+            contractType="CDI"
+            country="France"
+            city="Pantin"
+          />
+        </div>
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <p>
+          Made with <span className="bold">React</span> at{" "}
+          <span className="bold">Le Reacteur</span> by{" "}
+          <span className="bold">Claire Leconte</span>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
